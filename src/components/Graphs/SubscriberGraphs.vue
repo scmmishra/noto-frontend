8ab8a8af83fea03fcedcc2d1a5439ed0805dea1f
Shivam Mishra <scm.mymail@gmail.com>
2022-11-29 19:16:50 +0530

    feat: make subscriber graph

--- src/components/Graphs/SubscriberGraphs.vue
+++ src/components/Graphs/SubscriberGraphs.vue
@@ -1,12 +1,20 @@
 <script lang="ts" setup>
 import { formatDate, subtractDate } from "@/utils/date";
+import { ref, onMounted } from "vue";
 
 // generate an array of random numbers
-const subscriptions = Array.from({ length: 30 }, () =>
+let subscriptions = Array.from({ length: 30 }, () =>
   Math.floor(Math.random() * 100)
 );
 
+const maxHeight = ref("0px");
 const maxValue = Math.max(...subscriptions);
+
+onMounted(() => {
+  setTimeout(() => {
+    maxHeight.value = `500px`;
+  }, 100);
+});
 
 function getHeight(value: number) {
   return (value / maxValue) * 200;
@@ -17,15 +25,15 @@ function getHeight(value: number) {
   <div class="grid grid-cols-[repeat(30,_minmax(0,_1fr))] px-6 h-60">
     <div
       v-for="count in subscriptions"
-      class="border-dashed border-r first:border-l border-black-200 flex items-end justify-center hover:bg-black-100"
+      class="border-dotted border-r first:border-l border-black-200 flex items-end justify-center hover:bg-black-100"
     >
       <div
-        :style="{ height: `${getHeight(count)}px` }"
-        class="w-2 rounded-full bg-blue-500 my-2 transition-all duration-1000 h-0"
+        :style="{ height: `${getHeight(count)}px`, maxHeight }"
+        class="w-2 rounded-full bg-pink-500 my-2 transition-all duration-300 ease-in-out"
       ></div>
     </div>
   </div>
-  <div class="grid grid-cols-2 text-xs text-black-600 px-6">
+  <div class="grid grid-cols-2 text-xs text-black-600 px-6 mt-2">
     <time>{{ formatDate(subtractDate(30)) }}</time>
     <time class="text-right">{{ formatDate() }}</time>
   </div>
