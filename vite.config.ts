6f7da7a288c1467b1fef46ef98a4121be407cb99
Shivam Mishra <scm.mymail@gmail.com>
2022-11-29 18:07:12 +0530

    chore: update font plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePluginFonts } from "vite-plugin-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePluginFonts({
+      google: {
+        preconnect: true,
+        families: [
+          {
+            name: "Inter",
+            styles: "wght@400;500;600;700",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
