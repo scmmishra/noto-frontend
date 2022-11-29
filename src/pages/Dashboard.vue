2d7f5d40fb00db806c4ccc2e7c438d9365a3d4da
Shivam Mishra <scm.mymail@gmail.com>
2022-11-29 17:48:50 +0530

    feat: adjust fonts

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { formatDate } from "../utils/date";
+import Avatar from "../components/Base/Avatar.vue";
 
 const publishedPosts = [
   {
@@ -17,7 +18,7 @@ const publishedPosts = [
     title: "Audience feedback & Analytics",
     shortDescription:
       "Audience feedback is one of the most requested features on Splitbee. We are excited to announce that we are working on it.",
-    publishedOn: "2022-11-29T04:28:54.764Z",
+    publishedOn: "2022-11-16T04:28:54.764Z",
     author: {
       firstName: "Shivam",
       lastName: "Mishra",
@@ -28,7 +29,18 @@ const publishedPosts = [
     title: "Better reportings and visualizations",
     shortDescription:
       "With reporting you can see how your users are interacting with your website. You can see how many users are on your website, how many are new, and how many are returning.",
-    publishedOn: "2022-11-29T04:28:54.764Z",
+    publishedOn: "2022-10-28T04:28:54.764Z",
+    author: {
+      firstName: "Shivam",
+      lastName: "Mishra",
+      avatar: "https://randomuser.me/api/portraits/men/1.jpg",
+    },
+  },
+  {
+    title: "Multilingual support",
+    shortDescription:
+      "With reporting you can see how your users are interacting with your website. You can see how many users are on your website, how many are new, and how many are returning.",
+    publishedOn: "2022-10-18T04:28:54.764Z",
     author: {
       firstName: "Shivam",
       lastName: "Mishra",
@@ -40,42 +52,48 @@ const publishedPosts = [
 
 <template>
   <main class="grid space-y-6 py-8">
-    <section class="space-y-2 px-6">
+    <section class="space-y-2 px-6 flex items-center justify-between">
       <h1 class="text-2xl leading-8 text-black-999 font-extrabold">
         Changelogs
       </h1>
     </section>
     <section>
       <div
-        class="grid grid-cols-4 gap-5 py-5 uppercase text-xs tracking-wider font-semibold text-black-400 px-6"
+        class="grid grid-cols-4 gap-5 py-5 uppercase text-[13px] tracking-wider font-semibold text-black-400 px-6"
       >
         <div class="col-span-2">Title</div>
         <div class="col-span-1 text-right">Author</div>
         <div class="col-span-1 text-right">Published On</div>
       </div>
-      <div
-        v-for="post in publishedPosts"
-        class="grid grid-cols-4 gap-5 py-4 first:pt-0 border-t border-black-200 last:border-b hover:bg-black-100/50 px-6"
-      >
-        <div class="col-span-2">
-          <h3 class="text-black-800 text-lg font-semibold">
-            {{ post.title }}
-          </h3>
-          <p class="truncate text-black-600">
-            {{ post.shortDescription }}
-          </p>
-        </div>
-        <div class="col-span-1 flex items-center justify-end">
-          <span class="text-base text-black-600">
-            {{ post.author.firstName }} {{ post.author.lastName }}
-          </span>
-        </div>
-        <div class="col-span-1 flex items-center justify-end">
-          <time class="text-base text-black-600">
-            {{ formatDate(post.publishedOn) }}
-          </time>
-        </div>
-      </div>
+      <ul>
+        <li
+          v-for="post in publishedPosts"
+          class="grid grid-cols-4 gap-5 py-4 border-t border-black-200 last:border-b hover:bg-black-100/50 px-6 cursor-pointer"
+        >
+          <div class="col-span-2">
+            <h3 class="text-black-800 text-lg font-semibold">
+              {{ post.title }}
+            </h3>
+            <p class="truncate text-black-600 text-sm tracking-wide">
+              {{ post.shortDescription }}
+            </p>
+          </div>
+          <div class="col-span-1 flex items-center justify-end">
+            <span class="text-sm text-black-600 flex items-center gap-2">
+              <Avatar
+                :image="post.author.avatar"
+                :name="post.author.firstName"
+              ></Avatar>
+              {{ post.author.firstName }} {{ post.author.lastName }}
+            </span>
+          </div>
+          <div class="col-span-1 flex items-center justify-end">
+            <time class="text-sm text-black-600">
+              {{ formatDate(post.publishedOn) }}
+            </time>
+          </div>
+        </li>
+      </ul>
     </section>
   </main>
 </template>
