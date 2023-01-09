dfb6d09f104c9cff1b40525278fcdbae33281fd7
Shivam Mishra <scm.mymail@gmail.com>
2023-01-09 22:44:06 +0530

    chore: delete unused component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "@pages/Dashboard.vue";
 import Subscribers from "@pages/Subscribers.vue";
-import EditChangelog from "@/pages/EditChangelog.vue";
-import NewChangeLog from "@/pages/NewChangeLog.vue";
+import EditChangelog from "@pages/EditChangelog.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", name: "home", component: Dashboard },
-    { path: "/changelog/new", name: "new-log", component: NewChangeLog },
     { path: "/changelog/:id", name: "edit-log", component: EditChangelog },
     { path: "/subscribers", name: "subscribers", component: Subscribers },
   ],
